b695b22e353428b26e1408fd8b3f42840cb16a3a
MarkDLR <mark.p.dlr@gmail.com>
2020-06-15 17:56:34 -0500

    index works with all models

---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+Store.destroy_all
+Brand.destroy_all
+Shoe.destroy_all
+
+galleria = Store.create(store_name: "Galleria", store_location: "Houston") #12
+Store.create(store_name: "Memorial Ctiy", store_location: "Houston")
+Store.create(store_name: "NY Mall", store_location: "New York") #14
+Store.create(store_name: "Walmart", store_location: "Houston")
+Store.create(store_name: "Academy", store_location: "Houston") #16
+
+adidas = Brand.create(brand_name: "Adidas") #26
+Brand.create(brand_name: "Nike") #27
+Brand.create(brand_name: "Crocs") #28
+Brand.create(brand_name: "Jordans")
+Brand.create(brand_name: "Shaqs")
+Brand.create(brand_name: "converse")
+Brand.create(brand_name: "Puma") #32
+Brand.create(brand_name: "Reebok")
+Brand.create(brand_name: "Vans")
+Brand.create(brand_name: "Under Armour")
+Brand.create(brand_name: "New Balance")
+Brand.create(brand_name: "Skechers") #37
+
+light_race = Shoe.create(shoe_name:"light racer", shoe_color: "red", size: 11, price: 60, brand_id: 1, store_id: 1)
+
+20.times do 
+    Shoe.create({
+        shoe_name: ['Ultraboost', 'Winter Boots', 'light race', 'NMD', 'Superstar', 'Night Jogger', 'Copa 2020'].sample,
+        shoe_color: ['Purple', 'Grey', 'Black', 'Blue', 'Yellow', 'Pink', 'White', 'Brown', 'Red'].sample,
+        size: rand(5..12),
+        price: rand(100..200),
+        brand_id: Brand.all.sample.id,
+        store_id: Store.all.sample.id
+    })
+end
--- index.rb
+++ index.rb
@@ -1,3 +1,73 @@
 require_relative './config/environment.rb'
+# require 'pry'
+ActiveRecord::Base.logger = nil
 
+class CLI
 
+    def initialize
+        self.store_selection()
+    end
+
+    def store_selection
+        puts ''
+        puts "Welcome, please select store"
+        @user_location = gets.chomp()
+        @user_store_selection = Store.find_by({store_name: @user_location})
+        if !Store.all.include? @user_store_selection
+            puts ''
+            puts "There are no stores around #{@user_location}, our apologies for the inconvenience"
+            return self.store_selection()
+        else
+            puts ''
+            puts "Great"
+            self.brand_selection()
+        end
+    end
+
+    def brand_selection
+        puts ''
+        puts "What brand of shoes, are you looking for?"
+        @user_choice = gets.chomp()
+        @user_brand_choice = Brand.find_by({brand_name: @user_choice})
+        @shoe_results = Shoe.where({ store_id: @user_store_selection.id, brand_id: @user_brand_choice.id})
+    #"Adidas"
+        if !Brand.all.include? @user_brand_choice
+        #if @user_brand_choice.length == 0
+            puts ''
+            puts "Sorry that brand is not available at this moment."
+            return self.brand_selection()
+        else
+            puts ''
+            puts "Great"
+            return print_shoes()
+            #found_shoes = Shoe.where({brand_id: user_brand_choice.id})
+        end
+    end
+    #if found_id = Brand.where({brand_name: user_brand_choice})
+    #return Shoe.find_all({brand_id: found_id })
+    
+
+    def print_shoes
+        #shoes = []
+        puts ''
+        puts "This are all the shoes we have available"
+        # Brand.find_by({brand_name: @user_choice})
+        #Shoe.all.each do |shoe|
+            #if shoe.store_id == @user_store_selection.id && shoe.brand_id == @user_brand_choice.id
+            #if @user_store_selection.id == shoe.store_id && @user_brand_choice.id == shoe.brand_id
+             #   shoes << shoe 
+                #@user_brand_choice.shoes
+            if @shoe_results.length == 0
+                puts "There are no shoes of that brand at this location."
+                return print_shoes()
+            else
+                puts "These are all the shoes of that brand this location has."
+            end
+        #end
+        # binding.pry
+        p @shoe_results
+    end
+
+end
+
+CLI.new()
